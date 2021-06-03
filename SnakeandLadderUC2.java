df94480041eb072714b15eac52c5a1e8cd9ceb90
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-03 08:53:02 +0530

    Added :Roll Dice to get a Number

---
+++ SnakeandLadderUC2.java
@@ -0,0 +1,11 @@
+
+public class SnakeandLadderUC2 {
+	public static void main(String[] args) {
+		int SIDES =6;
+		int position = (int) ((Math.random() * SIDES) + 1);
+		System.out.println(position);
+	}
+
+}
+
+
